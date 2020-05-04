89e5736cc839ecae5fccfa69c82b1daa584b9a38
gregorio <gregorioenrique14@gmail.com>
2020-05-04 00:58:08 -0500

    vendor_csss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,10 @@
        
         <title>@yield('title') | Tecnologia</title>
   
-        <link href="{{ asset('css/admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/admin/vendor1/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
         <!-- Custom styles for this page -->
-        <link href="{{ asset('css/admin/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/admin/vendor1/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
         <!-- Custom styles for this template-->
         <link href="{{ asset('css/admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
        
@@ -46,27 +46,27 @@
         @yield('script')
 
         <!-- Bootstrap core JavaScript-->
-        <script src="{{ asset('css/admin/vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('css/admin/vendor1/jquery/jquery.min.js') }}"></script>
         <script src="{{ asset('js/app.js') }}"></script>
-        <script src="{{ asset('css/admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('css/admin/vendor1/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
        
     
         <!-- Core plugin JavaScript-->
-        <script src="{{ asset('css/admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+        <script src="{{ asset('css/admin/vendor1/jquery-easing/jquery.easing.min.js') }}"></script>
 
         <!-- Custom scripts for all pages-->
         <script src="{{ asset('css/admin/js/sb-admin-2.min.js') }}"></script>
 
         <!-- Page level plugins -->
-        <script src="{{ asset('css/admin/vendor/chart.js/Chart.min.js') }}"></script>
+        <script src="{{ asset('css/admin/vendor1/chart.js/Chart.min.js') }}"></script>
 
         <!-- Page level custom scripts -->
         <script src="{{ asset('css/admin/js/demo/chart-area-demo.js') }}"></script>
         <script src="{{ asset('css/admin/js/demo/chart-pie-demo.js') }}"></script> 
     
         <!-- Page level plugins -->
-        <script src="{{ asset('css/admin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset('css/admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+        <script src="{{ asset('css/admin/vendor1/datatables/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ asset('css/admin/vendor1/datatables/dataTables.bootstrap4.min.js') }}"></script>
         <script src="{{ asset('css/admin/js/demo/datatables-demo.js') }}"></script>
  
 
